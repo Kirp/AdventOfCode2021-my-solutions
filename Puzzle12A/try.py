389c0ae4adfc55b25fb47ce6d009668ea1c6af93
Kierro DeCampanille <pentawick@gmail.com>
2021-12-19 00:17:22 +0800

    Update try.py

--- Puzzle12A/try.py
+++ Puzzle12A/try.py
@@ -20,7 +20,32 @@ for read in readed:
     cleaned.append(read.split("\n")[0])
 #--
 
+def GetHighestIntPositionInList(intList):
+    highestPosi = 0
+    for elemCtr in range(len(intList)):
+        if intList[elemCtr] > intList[highestPosi]:
+            highestPosi = elemCtr
+    return highestPosi
+
+def GetScore(scoreStr, pathList):
+    #assume there are no invalid characters
+    if scoreStr == "start":
+        return -1
+    if scoreStr == "end":
+        return 1
+    if scoreStr.isupper() == True:
+        return 2
+    if pathList.count(scoreStr) > 0:
+        return -1
+    return 3
+
+
+
+
+
+
 #Main starts here
+#fill up the list
 nodeList = {}
 for clean in cleaned:
     dataSplit = clean.split("-")
@@ -33,9 +58,46 @@ for clean in cleaned:
         nodeList[dataSplit[1]].append(dataSplit[0])
     except:
         nodeList[dataSplit[1]] = [dataSplit[0]]
-    
 
-print(str(len(nodeList)))
-for node in nodeList:
-    print(node)
-    print(nodeList[node])
+#lets do the pathing
+# first lets add the links from start
+currentPathSteps = [["start"]]
+currentNode = "start"
+currentAvailablePaths = nodeList[currentNode]
+howManyPaths = len(currentAvailablePaths)
+for pathsCtr in range(howManyPaths):
+    pathList = [currentNode]
+    pathList.append(currentAvailablePaths[pathsCtr])
+    if pathsCtr < len(currentAvailablePaths)-1:
+        currentPathSteps[pathsCtr] = pathList
+    else:
+        currentPathSteps.append(pathList)
+print(currentPathSteps)
+
+howManyPaths = len(currentAvailablePaths)
+for pathsCtr in range(howManyPaths):
+    nodeLatestPath = currentAvailablePaths[pathsCtr]
+    nodeLatestPath = nodeLatestPath[len(nodeLatestPath)-1]
+    print("Latest Path: "+str(nodeLatestPath))  
+    try:  
+        nodeAvailablePaths = nodeList[nodeLatestPath]
+    except:
+        nodeAvailablePaths = []
+    print(nodeAvailablePaths)
+
+    #lets score them
+    scoreList = []
+    for elem in nodeAvailablePaths:
+        scoreList.append(GetScore(elem, nodeAvailablePaths))
+    print(scoreList)
+    highestScore = GetHighestIntPositionInList(scoreList)
+    print(highestScore)
+    chosenPath = nodeAvailablePaths[highestScore]
+    print(chosenPath)
+
+    #check if chosenPath has nodes 
+    try:
+        chosenPathCheck = nodeList[chosenPath]
+
+    except:
+        pass
